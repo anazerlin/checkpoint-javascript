2a4d0130cbb99db170a87eddf0013db9b6844d32
GabrielMachadoCP <rm99880@fiap.com.br>
2023-03-31 21:58:22 -0300

    Exercício 6 terminado

---
+++ js/script6.js
@@ -0,0 +1,7 @@
+let nota1 = parseFloat(prompt("Digite sua primeira nota:"))
+let nota2 = parseFloat(prompt("Digite sua segunda nota:"))
+let nota3 = parseFloat(prompt("Digite sua terceira nota:"))
+let nota4 = parseFloat(prompt("Digite sua quarta nota:"))
+let soma = (nota1 + nota2 + nota3 + nota4)
+let media = soma / 4
+console.log("Sua média é", media)
