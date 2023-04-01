1c8b85169859d15dea9b5e6aea04591633df7622
GabrielMachadoCP <rm99880@fiap.com.br>
2023-03-31 21:28:57 -0300

    Exercício 7 finalizado

---
+++ js/script7.js
@@ -0,0 +1,12 @@
+function mostraAviso()
+{
+    document.body.style.backgroundColor = 'green';
+}
+
+window.onload= function(){
+    let botaoAviso = document.getElementById("botao");
+
+    botaoAviso.onclick = function () { 
+        mostraAviso() ;
+    };
+}
