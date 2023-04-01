27a966ca23d3a6a7ea495b3dbda7e2ab203b0b7b
ana <analaura.chipweb@gmail.com>
2023-04-01 11:06:42 -0300

    realizando ex 6

--- js/script6.js
+++ js/script6.js
@@ -4,4 +4,4 @@ let nota3 = parseFloat(prompt("Digite sua terceira nota:"))
 let nota4 = parseFloat(prompt("Digite sua quarta nota:"))
 let soma = (nota1 + nota2 + nota3 + nota4)
 let media = soma / 4
-console.log("Sua média é", media)
+console.log("A sua média em notas é:", media)
