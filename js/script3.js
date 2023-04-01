6fbab75772c95b7e9ba4c832fb337f6e8aba77ea
ana <analaura.chipweb@gmail.com>
2023-04-01 10:52:06 -0300

    corrigindo ex 3

--- js/script3.js
+++ js/script3.js
@@ -1,14 +1,12 @@
-let altura = parseFloat(prompt("Digite sua altura em metros: "));
-let peso = parseFloat(prompt("Digite seu peso em quilogramas: "));
 
-let imc = peso / (altura ** 2); 
+let altura = parseFloat(prompt("Digite sua altura em metros:"))
+let peso = parseFloat(prompt("Digite seu peso em Kg:"))
+let imc = peso / (altura * altura)
 
-if (imc < 18.5) {
-  console.log("você está abaixo do peso ideal.");
-} else if (imc >= 18.5 && imc <= 24.9) {
-  console.log("você está no peso ideal.");
-} else {
-  console.log("você está acima do peso ideal.");
+if (imc < 18.5){
+    console.log("Você está abaixo do seu peso ideal!")
+}else if (imc > 24.9){
+    console.log("Você está acima do seu peso ideal!")
+}else{
+    console.log("Parabéns! Você está no seu peso ideal!")
 }
-
-//vou me matar hoje as 22:99
