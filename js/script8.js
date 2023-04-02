649f6192523058349b155eb9cacd73d413e247bd
ana <analaura.chipweb@gmail.com>
2023-04-02 11:09:51 -0300

    ex8 finalizado

--- js/script8.js
+++ js/script8.js
@@ -1,11 +1,11 @@
-var divElemento = document.getElementById("pg8")
+var divElemento = document.getElementById("ex8")
 
 divElemento.addEventListener("mouseover", ()=>{
     divElemento.style.backgroundColor = 'pink'
-    document.getElementById("pg8").innerHTML = "O mouse está sob a área do conteúdo";
+    document.getElementById("ex8").innerHTML = "O mouse está sob a área do conteúdo";
 })
 
 divElemento.addEventListener("mouseleave", ()=>{
     divElemento.style.backgroundColor = 'lightgreen'
-    document.getElementById("pg8").innerHTML = "O mouse está fora da área do conteúdo.!";
+    document.getElementById("ex8").innerHTML = "O mouse está fora da área do conteúdo.!";
 })
